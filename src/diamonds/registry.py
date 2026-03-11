7801146ca633eaf8d72203215a9a9eac5db75154
Vion Alexandre <alenvion@gmail.com>
2026-03-11 14:45:18 +0000

    Update save and load functions

--- src/diamonds/registry.py
+++ src/diamonds/registry.py
@@ -1,17 +1,17 @@
 import pickle 
 import os 
 from sklearn.base import BaseEstimator
-from diamonds.params import MODEL_FOLDER
+from diamonds.params import MODEL_PATH
 
 def save_model(estimator: BaseEstimator, name: str):
     """Save the model to the specified path."""
-    estimator_path = os.path.join(MODEL_FOLDER, f"{os.name}.pkl")
+    estimator_path = os.path.join(MODEL_PATH, f"{os.name}.pkl")
     with open(estimator_path, "wb") as f:
         pickle.dump(estimator, f)
 
 def load_model(name: str) -> BaseEstimator:
     """Load the model from the specified path."""
-    estimator_path = os.path.join(MODEL_FOLDER, f"{name}.pkl")
+    estimator_path = os.path.join(MODEL_PATH, f"{name}.pkl")
     with open(estimator_path, "rb") as f:
         estimator = pickle.load(f)
     return estimator
