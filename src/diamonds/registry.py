b5563addee5bffc7d71df4808b9399750528a70f
Vion Alexandre <alenvion@gmail.com>
2026-03-11 14:26:13 +0000

    Update save and load functions

--- src/diamonds/registry.py
+++ src/diamonds/registry.py
@@ -1,22 +1,17 @@
 import pickle 
 import os 
 from sklearn.base import BaseEstimator
-from diamonds.params import MODEL_REGISTRY 
+from diamonds.params import MODEL_FOLDER
 
-def save_model(model, path):
+def save_model(estimator: BaseEstimator, name: str):
     """Save the model to the specified path."""
-    # Implement the logic to save the model (e.g., using pickle, joblib, etc.)
-    model_path = "models"
-    if not os.path.exists(model_path) : 
-        os.mkdir(model_path)
-    with open(os.path.join(model_path,"preproc.pkl"),"wb") as f:
-    pickle.dump(preprocessor,f)
-    with open(os.path.join(model_path,"model.pkl"),"wb")  as f:
-    pickle.dump(forest,f)
+    estimator_path = os.path.join(MODEL_FOLDER, f"{os.name}.pkl")
+    with open(estimator_path, "wb") as f:
+        pickle.dump(estimator, f)
 
-def load_model(path) -> BaseEstimator:
+def load_model(name: str) -> BaseEstimator:
     """Load the model from the specified path."""
-    # Implement the logic to load the model (e.g., using pickle, joblib, etc.)
-    with open(os.path.join(model_path,"preproc.pkl"),"rb")  as f:
-        new_preproc = pickle.load(f)
-    new_preproc
+    estimator_path = os.path.join(MODEL_FOLDER, f"{name}.pkl")
+    with open(estimator_path, "rb") as f:
+        estimator = pickle.load(f)
+    return estimator
