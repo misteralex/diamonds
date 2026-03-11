d96a5beeefea8b864a27bfc01a0bb156f832e92b
Vion Alexandre <alenvion@gmail.com>
2026-03-11 14:47:02 +0000

    Added logs in registry.py

--- src/diamonds/registry.py
+++ src/diamonds/registry.py
@@ -1,17 +1,22 @@
 import pickle 
 import os 
+import logoru
 from sklearn.base import BaseEstimator
 from diamonds.params import MODEL_PATH
+
+logger = logoru.Logoru("registry")
 
 def save_model(estimator: BaseEstimator, name: str):
     """Save the model to the specified path."""
     estimator_path = os.path.join(MODEL_PATH, f"{os.name}.pkl")
     with open(estimator_path, "wb") as f:
         pickle.dump(estimator, f)
+    logger.info(f"Model saved to {estimator_path}")
 
 def load_model(name: str) -> BaseEstimator:
     """Load the model from the specified path."""
     estimator_path = os.path.join(MODEL_PATH, f"{name}.pkl")
     with open(estimator_path, "rb") as f:
         estimator = pickle.load(f)
+    logger.info(f"Model loaded from {estimator_path}")
     return estimator
